1b53f2680523d912011132780520e3aecf28dfcb
Dmitry Pimenov <dmitryspimenov@gmail.com>
2017-09-23 11:34:14 -0700

    added config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const Config =
+{
+    redirectUri: 'localhost:3000',
+    clientID: '<your-client-id>',
+    scopes: ['openid', 'profile', 'User.Read', 'Device.Read', 'Device.Command'],
+};
+
+export default Config;
